3d0a1b14cc44cfce5578d074ff2510ce731b3a54
Demezy <38487319+Demezy@users.noreply.github.com>
2023-01-30 21:27:55 +0300

    call rust function in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rust_bridge_bug_report/ffi.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +31,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _platform = "default";
 
   void _incrementCounter() {
     setState(() {
@@ -39,6 +41,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    api.platform().then(
+          (value) => _platform = value.toString(),
+        );
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -47,9 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            Text(_platform),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
